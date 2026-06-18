3cd6c30f86543151ab3590f2a08ba1064ca99d59
viquituor <202211190016@ifba.edu.br>
2026-06-18 14:50:06 -0300

    feat: adiciona modais de gerenciamento e componentes de formulário
    - Cria pop-ups unificados de cadastro/edição para Equipamentos, Manutenção e Usuários
    - Adiciona modais de ação do sistema (Empréstimo, Conclusão e Exclusão)
    - Implementa inputs e componentes de UI reutilizáveis (CaixaSelecao, DataSelecao, CaixaTexto e SubTitulo)

---
+++ frontend/src/components/PopUpCadastrarEditarEquipamento/index.jsx
@@ -0,0 +1,61 @@
+import TituloPagina from "../TituloPagina";
+import SubTitulo from "../SubTitulo";
+import CaixaTexto from "../CaixaTexto"
+import CaixaSelecao from "../CaixadeSelecao"
+import Botao from "../Botao"
+
+const categoriasDisponiveis = [
+  { valor: "computadores", texto: "Computadores e Notebooks" },
+  { valor: "perifericos", texto: "Periféricos (Mouse, Teclado, Monitor)" },
+  { valor: "audiovisual", texto: "Áudio e Vídeo (Projetores, Câmeras, Caixas de Som)" },
+  { valor: "redes", texto: "Redes e Conectividade (Roteadores, Switches, Cabos)" },
+  { valor: "ferramentas", texto: "Ferramentas de Manutenção" },
+  { valor: "laboratorio", texto: "Materiais de Laboratório (Multímetros, Placas, Componentes)" },
+  { valor: "acessorios", texto: "Acessórios Diversos (Adaptadores, Pen Drives)" }
+];
+
+
+export default function PopUpCadastrarEditarEquipamento({ modoEdicao = false }) {
+    return (
+        <div className="bg-white flex flex-col fixed top-[200px] w-[550px] items-center justify-around rounded-xl shadow-sm p-5">
+            
+            {/* 2. O Título muda dinamicamente usando chaves {} */}
+            <TituloPagina>
+                {modoEdicao ? "Editar Equipamento" : "Cadastrar Equipamento"}
+            </TituloPagina>
+            <SubTitulo>
+                {modoEdicao ? "Faça as alterações necessárias no formulário abaixo." : "Preencha o formulário abaixo para cadastrar um novo equipamento."}
+            </SubTitulo>
+
+            <form className="w-full flex flex-col gap-[10px]">
+                <CaixaTexto label="Nome" id="nome" placeholder="Digite o nome do equipamento" />
+                <CaixaSelecao label="Marca" id="marca" placeholder="Selecione a marca do equipamento" opcoes={categoriasDisponiveis} />
+                <CaixaTexto label="Nº do Patrimônio" id="patrimonio" placeholder="Digite o número do patrimônio do equipamento" />
+                <CaixaTexto label="Descrição" id="descricao" placeholder="Digite a descrição do equipamento" />
+                <CaixaSelecao label="Status" id="status" placeholder="Selecione o status do equipamento" opcoes={[
+                    { valor: "Disponível", texto: "Disponível" },
+                    { valor: "Em Manutenção", texto: "Em Manutenção" },
+                    { valor: "Indisponível", texto: "Indisponível" }
+                ]} />
+
+                <div className=" w-[500px] flex flex-row gap-6">
+                    <Botao
+                        children="Cancelar"
+                        onClick={() => console.log("Ação cancelada!")}
+                        type="button"
+                        estilo="cancelar"
+                        icone={false}
+                    />
+
+                    <Botao 
+                        children={modoEdicao ? "Salvar" : "Cadastrar"}
+                        onClick={() => console.log(modoEdicao ? "Equipamento editado!" : "Equipamento cadastrado!")}
+                        type="submit"
+                        estilo={modoEdicao ? "salvar" : "registrar"}
+                        icone={false}
+                    />
+                </div>
+            </form>
+        </div>
+    )
+}
--- frontend/src/pages/adm/admDashboard.jsx
+++ frontend/src/pages/adm/admDashboard.jsx
@@ -1,14 +1,21 @@
-import BarraLateral from "../../components/BarraLateral";
-import Cabecalho from "../../components/Cabecalho";
+import LayoutUsuario from "../../layouts/usuario";
+import PopUpCadastrarEditarEquipamento from "../../components/PopUpCadastrarEditarEquipamento";
+import PopUpCadastrarEditarManutenção from "../../components/PopUpCadastrarEditarManutenção";
+import PopUpCadastrarEditarUsuario from "../../components/PopUpCadastrarEditarUsuario";
+import PopUpEmprestimo from "../../components/PopUpEmprestimo";
+import PopUpConclusao from "../../components/PopupConclusao";
+import PopUpExclusao from "../../components/PopUpExclusao";
+import { use, useState } from "react";
+
 
 export default function AdmDashboard() {
     return (
         <section className="h-screen w-full flex row ">
-            <BarraLateral userRole="adm" />
-            <div className=" w-full flex col items-top justify-center">
-                <Cabecalho titulo="Dashboard" cargo="Administrador" nome="John Doe" />
+            <LayoutUsuario tipoUsuario="adm" titulo="Dashboard" cargo="Administrador" nome="John Doe">
 
-            </div>
+                
+                
+            </LayoutUsuario>
         </section>  
     );
 }
