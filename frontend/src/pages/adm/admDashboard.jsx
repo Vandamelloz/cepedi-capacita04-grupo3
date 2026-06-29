97177b09fa4c16e6a26d548ebeb8752984e09fca
viquituor <202211190016@ifba.edu.br>
2026-06-29 12:59:54 -0300

    feat(usuarios): implementa CRUD completo e feedback de ações

--- frontend/src/pages/adm/admDashboard.jsx
+++ frontend/src/pages/adm/admDashboard.jsx
@@ -1,11 +1,5 @@
 import LayoutUsuario from "../../layouts/usuario";
-import PopUpCadastrarEditarEquipamento from "../../components/PopUpCadastrarEditarEquipamento";
-import PopUpCadastrarEditarManutenção from "../../components/PopUpCadastrarEditarManutenção";
-import PopUpCadastrarEditarUsuario from "../../components/PopUpCadastrarEditarUsuario";
-import PopUpEmprestimo from "../../components/PopUpEmprestimo";
-import PopUpConclusao from "../../components/PopupConclusao";
-import PopUpExclusao from "../../components/PopUpExclusao";
-import { use, useState } from "react";
+
 
 
 export default function AdmDashboard() {
